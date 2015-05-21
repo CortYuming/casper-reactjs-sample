92b6558037604308d92a0a0dae36f649c4bcc07a
Takashi ITO <cortyuming@gmail.com>
2015-05-21 09:58:30 +0900

    phantomjs2をインストールしてcicleCIで実行できるようにする

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var runSequence = require('run-sequence');
 
-gulp.task('npm-install', shell.task([
-    'npm install'
-]));
+// gulp.task('npm-install', shell.task([
+//     'npm install'
+// ]));
 gulp.task('casperjs', shell.task([
     'node_modules/.bin/casperjs test e2e/'
 ]));
 gulp.task('e2e-test', function(callback) {
     runSequence(
-        'npm-install',
+        // 'npm-install',
         'casperjs',
         callback
     );
